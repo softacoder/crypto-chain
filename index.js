41df9137e1ea2cae14227cabaf37542ffc5880d5
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-01 04:56:47 +0100

    commit start peers and broadcast chain

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const Blockchain = require("./blockchain");
+const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -16,10 +20,19 @@ app.post("/api/mine", (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect("/api/blocks");
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
 });
