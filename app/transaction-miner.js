c1f3154b5946ea25afe3331df2577b4df0bc15fc
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-05 12:34:53 +0100

    commit miner class

---
+++ app/transaction-miner.js
@@ -0,0 +1,12 @@
+class TransactionMiner {
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.pubsub = pubsub;
+  }
+
+  mineTransacions() {}
+}
+
+module.exports = TransactionMiner;
