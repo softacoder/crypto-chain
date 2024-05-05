f9ae3670315c0b6672f31ffd29166f9bb847d986
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-05 02:14:02 +0100

    commit updates in API

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -6,5 +6,13 @@ class TransactionPool {
   setTransactionMap(transaction) {
     this.transactionMap[transaction.id] = transaction;
   }
+
+  existingTransaction({ inputAddress }) {
+    const transaction = Object.values(this.transactionMap);
+
+    return transactions.find(
+      (transaction) => transaction.input.address === inputAddress
+    );
+  }
 }
 module.exports = TransactionPool;
