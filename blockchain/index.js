9fadc539cad5c1ff340ce279247685993578ee16
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-07 08:53:19 +0100

    commit clear recorded transactions

--- blockchain/index.js
+++ blockchain/index.js
@@ -13,7 +13,7 @@ class Blockchain {
     this.chain.push(newBlock);
   }
 
-  replaceChain(chain) {
+  replaceChain(chain, onSuccess) {
     if (chain.length <= this.chain.length) {
       console.error("The incoming chain must be longer");
 
@@ -26,6 +26,7 @@ class Blockchain {
       return;
     }
 
+    if (onSuccess) onSuccess();
     console.log("replacing chain with", chain);
     this.chain = chain;
   }
