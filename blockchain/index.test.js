b3395d0aa9ac4ea22730fed121899b62dee4058e
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-08 10:32:11 +0100

    commit transaction data test

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,6 +1,8 @@
 const Blockchain = require("./index");
 const Block = require("./block");
 const { cryptoHash } = require("../util");
+const Wallet = require("../wallet");
+const Transaction = require("../wallet/transaction");
 
 describe("Blockchain", () => {
   let blockchain;
@@ -156,6 +158,65 @@ describe("replaceChain()", () => {
       it("logs about the chain replacement", () => {
         expect(logMock).toHaveBeenCalled();
       });
+    });
+  });
+});
+
+describe("validTransactionData()", () => {
+  let transaction, rewardTransaction, wallet;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+    transaction = wallet.createTransaction({
+      recipient: "foo-address",
+      amount: 65,
+    });
+    rewardTransaction = Transaction.rewardTransaction({
+      address: wallet.publicKey,
+    });
+
+    describe("and the transaction data is valid", () => {
+      it("returns true", () => {
+        newChain.addBlock({ data: [transaction, rewardTransaction] });
+
+        expect(
+          blockchain.validTransactionData({ chain: newChain.chain }).toBe(true)
+        );
+      });
+    });
+
+    describe("and the transaction data has multiple rewards", () => {
+      it("returns true", () => {
+        newChain.addBlock({ data: [transaction, rewardTransaction] });
+
+        expect(blockchain.validTransactionData({ chain: newChain })).toBe(true);
+      });
+    });
+
+    describe("and the transaction data has at least one malformed outputMap", () => {
+      describe("and the transaction is not a reward transaction", () => {
+        it("returns false", () => {
+          transaction.outputMap[wallet.publicKey] = 999999;
+
+          newChain.addBlock({ data: [transaction, rewardTransaction] });
+
+          expect(blockchain.validTransactionData({ chain: newChain })).toBe(
+            false
+          );
+        });
+
+        describe("and the transaction is a reward transaction", () => {
+          it("returns false", () => {});
+        });
+      });
+
+      describe("and the transaction has at least one malformed input", () => {
+        it("returns false", () => {});
+      });
+    });
+
+    describe("and a block contains multiple identical transactions", () => {
+      it("returns false", () => {});
     });
   });
 });
