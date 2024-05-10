2fd4b4f1cc171f052b4122ea0c7e4cb6c1ba4615
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-10 13:12:38 +0100

    commit display wallet-info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = { walletInfo: { address: "fooxv6", balance: 9999 } };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/wallet-info")
+      .then((response) => response.json())
+      .then((json) => this.setState({ walletInfo: json }));
+  }
+
   render() {
-    return <div>Welcome to the blockchain...</div>;
+    const { address, balance } = this.state.walletInfo;
+
+    return (
+      <div>
+        <div>Welcome to the blockchain...</div>
+        <div>Address: {address}</div>
+        <div>Balance: {balance}</div>
+      </div>
+    );
   }
 }
 
