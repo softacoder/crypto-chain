9f629282536c37d13132fecb43c377f6c5f3fbbe
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-13 01:41:03 +0100

    commit transaction component

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,33 +1,23 @@
 import React, { Component } from "react";
-// import Blocks from "blocks";
 import Block from "./Block";
 
 class Blocks extends Component {
   state = { blocks: [] };
-
   componentDidMount() {
     fetch("http://localhost:3000/api/blocks")
       .then((response) => response.json())
       .then((json) => this.setState({ blocks: json }));
   }
-
   render() {
     console.log("this.state", this.state);
-
     return (
       <div>
         <h3>Blocks</h3>
         {this.state.blocks.map((block) => {
-          return (
-            <div key={block.hash} className="Block">
-              {block.hash}
-            </div>
-            <Block key={block.hash} block={block} /> 
-          );
+          return <Block key={block.hash} block={block} />;
         })}
       </div>
     );
   }
 }
-
 export default Blocks;
--- client/src/components/block.js
+++ client/src/components/block.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import Transaction from "./Transaction";
 
 class Block extends Component {
-  render() {
-    const { timestamp, hash, data } = this.props.block;
+  state = { displayTransaction: false };
 
-    const hashDisplay = `${hash.substring(0, 15)}...`;
+  toggleTransaction = () => {
+    this.setState({ displayTransaction: !this.state.displayTransaction });
+  };
+
+  get displayTransaction() {
+    const { data } = this.props.block;
+
+    toggleTransaction = () => {
+      this.setState({ displayTransaction: !this.state.displayTransaction });
+    };
+
     const stringifiedData = JSON.stringify(data);
 
     const dataDisplay =
@@ -12,11 +23,54 @@ class Block extends Component {
         ? `${stringifiedData.substring(0, 35)}...`
         : stringifiedData;
 
+    if (this.state.displayTransaction) {
+      return (
+        <div className="Transaction">
+          {data.map((transaction) => (
+            <div key={transaction.id}>
+              <hr />
+              <Transaction transaction={transaction} />
+            </div>
+          ))}
+          <br />
+          {JSON.stringify(data)}
+          <br />
+          <Button
+            bsStyle="danger"
+            bsSize="small"
+            onClick={this.toggleTransaction}
+          >
+            Show Less
+          </Button>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <div>Data: {dataDisplay}</div>
+        <Button
+          bsStyle="danger"
+          bsSize="small"
+          onClick={this.toggleTransaction}
+        >
+          Show More
+        </Button>
+      </div>
+    );
+  }
+
+  render() {
+    const { timestamp, hash } = this.props.block;
+
+    const hashDisplay = `${hash.substring(0, 15)}...`;
+
     return (
       <div className="Block">
         <div>Hash: {hashDisplay}</div>
         <div>Timestamp: {new Date(timestamp)}</div>
-        <div>Data: {dataDisplay}</div>
+        {this.displayTransaction}
+        {/* <div>Data: {dataDisplay}</div> */}
       </div>
     );
   }
